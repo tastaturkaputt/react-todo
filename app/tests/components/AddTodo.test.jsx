6ad46b13f4976a13749686fd5d4722d988bb6c03
Vitali Schneider <tastaturkaputt@googlemail.com>
2017-05-18 14:20:52 +0200

    Add AddTodo Component with tests

---
+++ app/tests/components/AddTodo.test.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var AddTodo = require('AddTodo');
+
+describe('AddTodo', () => {
+  it('should exist', () => {
+    expect(AddTodo).toExist();
+  });
+
+  it('should call onAddTodo if valid todo text entered', () => {
+    var spy = expect.createSpy();
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(addTodo));
+
+    addTodo.refs.todo.value = 'Valid Todo';
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toHaveBeenCalledWith('Valid Todo');
+  });
+
+  it('should not call onAddTodo if invalid todo text entered', () => {
+    var spy = expect.createSpy();
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(addTodo));
+
+    addTodo.refs.todo.value = '';
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toNotHaveBeenCalled();
+  });
+});
