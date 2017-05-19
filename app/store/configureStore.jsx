3878f8fc493875c1222aeb227296ade812e3dc52
Vitali Schneider <tastaturkaputt@googlemail.com>
2017-05-19 17:29:05 +0200

    Refactor TodoList ,Todo and AddTodo to work with reducers

--- app/store/configureStore.jsx
+++ app/store/configureStore.jsx
@@ -2,7 +2,7 @@ var redux = require('redux');
 //var thunk = require('redux-thunk').default;
 var {searchTextReducer, showCompletedReducer, todosReducer} = require('reducers');
 
-export var configure = () => {
+export var configure = (initialState = {}) => {
 
   // Combine Reducers
   // -------------------------
@@ -12,7 +12,7 @@ export var configure = () => {
     todos: todosReducer
   });
 
-  var store = redux.createStore(reducer, redux.compose(
+  var store = redux.createStore(reducer, initialState, redux.compose(
     //redux.applyMiddleware(thunk),
     window.devToolsExtension ? window.devToolsExtension() : f => f
   ));
