3294a5886a0ef7157154e3b72f9d61b50c150a54
Vitali Schneider <tastaturkaputt@googlemail.com>
2017-05-23 00:47:33 +0200

    Fetch todos from firebase on app start

--- app/app.jsx
+++ app/app.jsx
@@ -9,14 +9,8 @@ var actions = require('actions');
 var store = require('configureStore').configure();
 var TodoAPI = require('TodoAPI');
 
-store.subscribe(() => {
-  var state = store.getState();
-  console.log('New state', state);
-  TodoAPI.setTodos(state.todos);
-});
-
-var initialTodos = TodoAPI.getTodos();
-store.dispatch(actions.addTodos(initialTodos));
+// Fetch everything from firebase and add it to the store!
+store.dispatch(actions.startAddTodos());
 
 // Load foundation
 $(document).foundation();
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -12,56 +12,6 @@ describe('TodoAPI', () => {
     expect(TodoAPI).toExist();
   });
 
-  describe('setTodos', () => {
-
-    it('should set valid todos array', () => {
-      var todos = [{
-        id: 23,
-        text: 'Test all files',
-        completed: false
-      }];
-
-      TodoAPI.setTodos(todos);
-      var actualTodos = JSON.parse(localStorage.getItem('todos'));
-      expect(actualTodos).toEqual(todos);
-    });
-
-    it('should not set invalid todos array', () => {
-      var badTodos = {
-        id: 23,
-        text: 'Test all files',
-        completed: false
-      };
-
-      TodoAPI.setTodos(badTodos);
-      expect(localStorage.getItem('todos')).toBe(null);
-    });
-  });
-
-  describe('getTodos', () => {
-
-    it('should return empty array for bad localstorage data', () => {
-
-      var actualTodos = TodoAPI.getTodos('todos');
-      expect(actualTodos).toEqual([]);
-
-    });
-
-    it('should return filled array for valid localstorage data', () => {
-      var todos = [{
-        id: 23,
-        text: 'Test all files',
-        completed: false
-      }];
-
-      localStorage.setItem('todos', JSON.stringify(todos));
-      var actualTodos = TodoAPI.getTodos();
-
-      expect(actualTodos).toEqual(todos);
-    });
-
-  });
-
   describe('filterTodos', () => {
 
     var todos = [{
